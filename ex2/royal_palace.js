be168c79641f9743b17a7768cba6ef2dd0984fa1
andrewwb1 <aboettcher@unc.edu>
2025-01-22 12:31:15 -0500

    assignment 2 complete

---
+++ ex2/royal_palace.js
@@ -0,0 +1,8 @@
+var royal_palace = {
+"type": "FeatureCollection",
+"name": "royal_palace",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -3.714290767085134, 40.417927067583136 ] } }
+]
+}
