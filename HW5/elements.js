a40a38b71f1b6f68558249c75d44d5e61cd49540
Andrew Boettcher <aboettcher@unc.edu>
2025-02-23 19:46:11 -0500

    yes

---
+++ HW5/elements.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([39.828175, -98.5795], 4);
+
+var OSM = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
